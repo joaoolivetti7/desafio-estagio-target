74d86d46847b3159d3e46186bf037b27e67114a1
João Olivetti <joaoolivetti45@gmail.com>
2023-03-08 10:04:02 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let INDICE = 13
+let SOMA = 0
+let K = 0
+
+while (K < INDICE) {
+ K = K + 1
+ SOMA = SOMA + K
+}
+
+console.log('O valor da variável SOMA será a soma de todos os números de 1 a 13, que é 1 + 2 + 3 + ... + 12 + 13 =', SOMA)
